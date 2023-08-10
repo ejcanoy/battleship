0e0e6e5ae82800514e2a98a3d2cde21b3599249e
ejcanoy <euan.james.canoy@gmail.com>
2023-08-10 13:32:24 -0700

    created draft ui

--- webpack.config.js
+++ webpack.config.js
@@ -8,23 +8,23 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: ["node_modules"],
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.gif$/,
-        type: "asset/inline",
-      },
-      {
-        test: /\.(ttf|eot|svg)$/,
-        type: "asset/resource",
-      },
+    {
+      test: /\.jsx?$/,
+      exclude: /node_modules/, // Use a regular expression to exclude node_modules
+      use: ["babel-loader"],
+    },
+    {
+      test: /\.css$/,
+      use: ["style-loader", "css-loader"],
+    },
+    {
+      test: /\.gif$/,
+      type: "asset/inline",
+    },
+    {
+      test: /\.(ttf|eot|svg)$/,
+      type: "asset/resource",
+    },
     ],
   },
   resolve: {
@@ -34,15 +34,10 @@ module.exports = {
     },
     extensions: [".js", ".jsx"],
     modules: [
-      "node_modules",
-      "bower_components",
-      "shared",
-      "/shared/vendor/modules",
+    "node_modules",
+    "bower_components",
+    "shared",
+    "/shared/vendor/modules",
     ],
-  },
-  moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/__mocks__/fileMock.js",
-    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
   },
 };
